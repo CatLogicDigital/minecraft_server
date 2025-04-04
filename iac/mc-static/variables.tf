91ad0a0b5656479f978294cc9c5bc3e939a464fe
CatLogicDigital <hello@catlogic.digital>
2025-04-05 00:57:06 +0100

    Update variables.tf

--- iac/mc-static/variables.tf
+++ iac/mc-static/variables.tf
@@ -1,8 +1,3 @@
-# aws profile
-variable "aws-profile" {
-  type = string
-}
-
 # region
 variable "aws-region" {
   type = string
@@ -23,7 +18,7 @@ variable "ami-images" {
   type = map(string)
 
   default = {
-    "eu-central-1" = "ami-0233214e13e500f77"
+    "eu-west-2" = "ami-0233214e13e500f77"
   }
 }
 
@@ -32,7 +27,7 @@ variable "aws-zones" {
   type = map(string)
 
   default = {
-    "eu-central-1" = "eu-central-1a"
+    "eu-west-2" = "eu-west-2a"
   }
 }
 
