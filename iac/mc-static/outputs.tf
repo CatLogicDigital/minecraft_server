f92308ef89435a6f7ee936bd3dd22b4f5cbb80ec
CatLogicDigital <hello@catlogic.digital>
2025-04-05 03:35:35 +0100

    Update outputs.tf

--- iac/mc-static/outputs.tf
+++ iac/mc-static/outputs.tf
@@ -1,6 +1,6 @@
-#output "Auto_Shutoff_Topic" {
-#  value = "${aws_sns_topic.mc_shutoff.arn}"
-#}
+output "Auto_Shutoff_Topic" {
+  value = "${aws_sns_topic.mc_shutoff.arn}"
+}
 
 output "Minecraft_Public_IP" {
   value = "${aws_eip.mc_ip.public_ip}"
