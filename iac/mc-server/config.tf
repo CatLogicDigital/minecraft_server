3435c3367f98946f4e1cd32d4e1cc04bf0afa98f
CatLogicDigital <hello@catlogic.digital>
2025-04-05 21:33:15 +0100

    Update config.tf

--- iac/mc-server/config.tf
+++ iac/mc-server/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-catlogic"
-    key            = "mincraft-server.tfstate"
+    key            = "mincraft-server-running.tfstate"
     region         = "eu-west-2"
     encrypt        = true
   }
