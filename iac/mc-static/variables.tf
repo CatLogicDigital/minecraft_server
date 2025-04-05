aedb1c6e4a9b451998d44bccb0f9056f7e9d8a04
CatLogicDigital <hello@catlogic.digital>
2025-04-05 16:58:29 +0100

    Update variables.tf

--- iac/mc-static/variables.tf
+++ iac/mc-static/variables.tf
@@ -4,9 +4,8 @@ variable "tf-bucket" {
 }
 
 # define the region specific ami images
-variable "ami-images" {
-  type = map(string)
-
+#variable "ami-images" {
+#  type = map(string)
 #  default = {
 #    "eu-west-2" = "ami-0c00cb2f2d8ea8cc6" # Latest Amazon Linux AMI
 #  }
