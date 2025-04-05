0ef83bac99cc3faa0c3efb8c3fd0b9baa8a77a7e
CatLogicDigital <hello@catlogic.digital>
2025-04-05 23:51:37 +0100

    Update ec2_mc_server.tf

--- iac/mc-server/ec2_mc_server.tf
+++ iac/mc-server/ec2_mc_server.tf
@@ -1,167 +1,74 @@
-# ------------------------------------
-# Minecraft EC2 server
-# ------------------------------------
-
-# import existing ec2 keypair
-data "aws_key_pair" "existing" {
-  key_name = var.ec2-key-pair-name
-}
-
-# import s3 used for backup
-data "aws_s3_bucket" "mc_backup" {
-  bucket = var.mc-backup-bucket-name
-}
-
 resource "aws_instance" "minecraft" {
-  instance_type = "t2.medium"
+  ami                    = var.ami-images[var.aws-region]
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.minecraft.id
+  key_name               = data.aws_key_pair.existing.key_name
+  vpc_security_group_ids = [aws_security_group.minecraft.id]
+  associate_public_ip_address = true
 
-  ami               = var.ami-images[var.aws-region]
-  security_groups   = [aws_security_group.minecraft.id]
-  availability_zone = var.aws-zones[var.aws-region]
-  key_name          = data.aws_key_pair.existing.key_name
-  depends_on        = [aws_internet_gateway.minecraft]
-  subnet_id         = aws_subnet.minecraft.id
-
-  iam_instance_profile = aws_iam_instance_profile.minecraft.name
-
-  root_block_device {
-    volume_type = "standard"
-    volume_size = 40
+  tags = {
+    Name = "minecraft-server"
   }
 
-  tags        = local.common_tags
-  volume_tags = local.common_tags
-}
-
-resource "aws_eip_association" "minecraft" {
-  instance_id   = aws_instance.minecraft.id
-  allocation_id = data.aws_eip.mc_ip.id
-}
-
-# ------------------------------------
-# IAM Roile for mincraft world backup on S3
-# ------------------------------------
-resource "aws_iam_instance_profile" "minecraft" {
-  name = "mc-backup"
-  role = "${aws_iam_role.minecraft.name}"
-}
-
-resource "aws_iam_role" "minecraft" {
-  name = "mc-backup-role"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-               "Service": "ec2.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
-}
-EOF
-
-  tags = local.common_tags
-}
-
-resource "aws_iam_role_policy" "minecraft" {
-  name = "mc-backup-publish-policy"
-  role = "${aws_iam_role.minecraft.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": [
-        "SNS:Publish"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-# -----------------------------------------
-# Provision the minecraft server using remote-exec
-# -----------------------------------------
-# Download the private key from S3
-resource "null_resource" "minecraft" {
   provisioner "remote-exec" {
     inline = [
-      "chmod a+x mc-*.sh",
-      "./mc-setup.sh ${data.aws_s3_bucket.mc_bucket.id} ${data.aws_sns_topic.mc_shutoff.arn} ${var.aws-region}",
+      "sudo yum install -y java-17-amazon-corretto",
+      "aws s3 cp s3://${var.mc-backup-bucket-name}/minecraft-key.pem ~/minecraft-key.pem",
+      "chmod 600 ~/minecraft-key.pem",
+      "aws s3 cp s3://${var.mc-backup-bucket-name}/start.sh ~/start.sh",
+      "chmod +x ~/start.sh",
+      "~/start.sh"
     ]
 
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      host        = aws_instance.minecraft.public_ip
+      host        = self.public_ip
       private_key = file("${path.module}/minecraft-key.pem")
     }
   }
-
-  depends_on = [
-    aws_instance.minecraft,
-    null_resource.download_pem
-  ]
 }
 
-  // copy pre-configured ec2 instance private key
-  provisioner "file" {
-    source      = "~/.ssh/${var.ec2-key-pair-name}.pem"
-    destination = "id_rsa"
+resource "null_resource" "download_pem" {
+  provisioner "local-exec" {
+    command = "aws s3 cp s3://${var.mc-backup-bucket-name}/minecraft-key.pem ${path.module}/minecraft-key.pem"
+  }
+}
+
+data "aws_key_pair" "existing" {
+  key_name = var.ec2-key-pair-name
+}
+
+resource "aws_security_group" "minecraft" {
+  name        = "minecraft-sg"
+  description = "Allow Minecraft traffic"
+  vpc_id      = aws_vpc.minecraft.id
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // copy mc auto-shutoff function
-  provisioner "file" {
-    source      = "../../src/auto-shutoff.py"
-    destination = "auto-shutoff.py"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  // copy mc deployment and start script
-  provisioner "file" {
-    source      = "../../src/mc-setup.sh"
-    destination = "mc-setup.sh"
-  }
-  provisioner "file" {
-    source      = "../../src/mc-server.sh"
-    destination = "mc-server.sh"
-  }
+resource "aws_subnet" "minecraft" {
+  vpc_id                  = aws_vpc.minecraft.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = var.aws-zones[var.aws-region]
+  map_public_ip_on_launch = true
+}
 
-  // copy tf config and var template
-  provisioner "file" {
-    source      = "./config.tf"
-    destination = "config.tf"
-  }
-  provisioner "file" {
-    source      = "./variables.tf"
-    destination = "variables.tf"
-  }
-  provisioner "file" {
-    source      = "../../config/account.tfvars"
-    destination = "account.tfvars"
-  }
-
-  // install minecraft and sync backup
-  provisioner "remote-exec" {
-    inline = [
-      "chmod a+x mc-*.sh",
-      "./mc-setup.sh ${data.aws_s3_bucket.mc_bucket.id} ${data.aws_sns_topic.mc_shutoff.arn} ${var.aws-region}"
-    ]
+resource "aws_vpc" "minecraft" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "minecraft-vpc"
   }
 }
