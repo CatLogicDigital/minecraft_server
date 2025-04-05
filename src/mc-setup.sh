2398fd140d71b1ccf51d02c546d5c23ae8ad2cdd
CatLogicDigital <hello@catlogic.digital>
2025-04-06 00:26:32 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-set -eux
-
 # sudo yum update -y
-sudo yum install java-1.8.0 -y
-sudo yum remove java-1.7.0-openjdk -y
+#sudo yum install java-1.8.0 -y
+#sudo yum remove java-1.7.0-openjdk -y
+
+# install Java
+sudo apt install openjdk-19-jre-headless
 
 # copy tf templates to minecraft backup bucket
 aws s3 cp config.tf s3://$1
@@ -18,35 +18,42 @@ aws s3 sync s3://$1 minecraft/
 if [ ! -f "minecraft/eula.txt" ]; then
     echo "Installing Minecraft"
     cd minecraft
-        rm forge.jar || true
         # pick latest from https://www.minecraft.net/en-us/download/server/
         #wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
-        wget http://alx365.github.io/BTE_Official_Server_MAC.zip
-        unzip BTE_Official_Server_MAC.zip
-        rm BTE_Official_Server_MAC.zip
-        sh install.sh
+        wget wget https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar
+        # run the jar file for the first time. It will fail but don’t worry about it, that is expected behavior. Run this command:
+        java -Xmx1024M -Xms1024M -jar server.jar nogui
 
-        cat >eula.txt<<EOF
-#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
-#Tue Jan 27 21:40:00 UTC 2015
-eula=true
-EOF
+    echo "### Accepting EULA"
+    echo "eula=true" > eula.txt
+    
+    echo "### Setting server properties"
+    cat > server.properties <<EOF
+    difficulty=normal
+    gamemode=survival
+    level-name=world
+    motd=Welcome to CatLogic Minecraft
+    pvp=true
+    EOF
+
 
     cd ..
 fi
 
 # install minecraft status 
+echo "Installing Minecraft Status [mcstatus]"
 sudo pip install mcstatus
 
 # insert auto-shutoff into cron tab and run each minute
 crontab -l | { cat; echo "* * * * * python auto-shutoff.py s3://$1 $2 $3"; } | crontab -
 
 # start minecraft (does not return)
-cd minecraft
-    rm nohup.out || true
-    # copying from S3 drops the x perm
-    chmod a+x run_nogui.sh
-    nohup ./run_nogui.sh &
-    sleep 10
-    cat nohup.out
-cd ..
+screen java -Xmx1024M -Xms1024M -jar server.jar nogui
+#cd minecraft
+#    rm nohup.out || true
+#    # copying from S3 drops the x perm
+#    chmod a+x run_nogui.sh
+#    nohup ./run_nogui.sh &
+#    sleep 10
+#    cat nohup.out
+#cd ..
