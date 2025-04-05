f2d29d562edaef041a2db9ab37e768f3f56ca8ef
CatLogicDigital <hello@catlogic.digital>
2025-04-05 01:52:15 +0100

    Update s3_mc_backup.tf

--- iac/mc-static/s3_mc_backup.tf
+++ iac/mc-static/s3_mc_backup.tf
@@ -6,8 +6,3 @@ resource "aws_s3_bucket" "mc_backup" {
   bucket = "${local.prefix}-mc-backup" 
   tags = "${local.common_tags}"
 }
-
-resource "aws_s3_bucket_acl" "mc_backup_acl" {
-  bucket = aws_s3_bucket.mc_backup.id
-  acl    = "private"
-}
