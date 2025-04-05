45bbaa2d13347691b6cb9c47d84854905cb87d34
CatLogicDigital <hello@catlogic.digital>
2025-04-05 01:08:48 +0100

    Update s3_mc_backup.tf

--- iac/mc-static/s3_mc_backup.tf
+++ iac/mc-static/s3_mc_backup.tf
@@ -1,6 +1,9 @@
+provider "aws" {
+  region = var.aws-region
+}
+
 resource "aws_s3_bucket" "mc_backup" {
   bucket = "${local.prefix}-mc-backup"
-  region = "${var.aws-region}"
   acl    = "private"
   
   tags = "${local.common_tags}"
