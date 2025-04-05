954bea310a3983e724daad9cde788fc8b741242e
CatLogicDigital <hello@catlogic.digital>
2025-04-05 02:18:00 +0100

    Update config.tf

--- iac/mc-static/config.tf
+++ iac/mc-static/config.tf
@@ -5,13 +5,19 @@ terraform {
     region         = "eu-west-2"
     encrypt        = true
   }
-}
 
-terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     archive = {
       source  = "hashicorp/archive"
       version = "~> 1.0"
     }
   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
 }
