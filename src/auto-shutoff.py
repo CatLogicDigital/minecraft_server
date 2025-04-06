05b26aa97ba0092fed2e3e1b6c426e8f365c4553
CatLogicDigital <hello@catlogic.digital>
2025-04-06 01:59:10 +0100

    Update auto-shutoff.py

--- src/auto-shutoff.py
+++ src/auto-shutoff.py
@@ -1,63 +1,54 @@
-#sudo pip install mcstatus
 from mcstatus import MinecraftServer
 import time
-import json
 import os.path
-import urllib2
 import subprocess
 import sys
 
 def check_call(args):
-    """Wrapper for subprocess that checks if a process runs correctly,
-    and if not, prints stdout and stderr.
-    """
     proc = subprocess.Popen(args,
-		shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        cwd='/tmp')
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            cwd='/tmp')
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        print(stdout)
-        print(stderr)
-        raise subprocess.CalledProcessError(
-            returncode=proc.returncode,
-            cmd=args)
+        print(stdout.decode())
+        print(stderr.decode())
+        raise subprocess.CalledProcessError(proc.returncode, args)
 
 server = MinecraftServer.lookup("localhost:25565")
 status = server.status()
 
-# if last activity was recorded 
-if os.path.exists('/tmp/mc_last_activity'):
-	f = open('/tmp/mc_last_activity', 'r+')
+last_activity_file = '/tmp/mc_last_activity'
+backup_marker = '/tmp/mc_backup'
 
-	if status.players.online:
-		# if players are online, remove timestamp file
-		f.seek(0)
-		f.write(str(time.time()))
-		f.truncate()
+# Check for last recorded activity
+if os.path.exists(last_activity_file):
+    with open(last_activity_file, 'r+') as f:
+        if status.players.online:
+            # Players are online; update activity timestamp
+            f.seek(0)
+            f.write(str(time.time()))
+            f.truncate()
 
-		# force new backup on inactivity
-		if os.path.exists("/tmp/mc_backup"):
-  			os.remove("/tmp/mc_backup")
+            # Remove existing backup marker if players reconnect
+            if os.path.exists(backup_marker):
+                os.remove(backup_marker)
+        else:
+            old_time = float(f.read())
+            time_past = time.time() - old_time
 
-	else:
-		# get timestamp of last activity
-		old_time = float(f.read()) 
-		time_past = time.time() - old_time
+            # Check inactivity period (1 hour)
+            if time_past > (60 * 60):
+                if not os.path.exists(backup_marker):
+                    with open(backup_marker, 'w') as p:
+                        p.write(str(time.time()))
+                    check_call(f'aws s3 sync /home/ec2-user/minecraft/ {sys.argv[1]} --exclude logs/*')
 
-		# more than 1 hour of inactivity?
-		if time_past > (60*60):
-			# backup mc world 
-			if not os.path.exists('/tmp/mc_backup'):
-				p = open('/tmp/mc_backup', 'w')
-				p.write(str(time.time()))
-				check_call('aws s3 sync /home/ec2-user/minecraft/ {0} --exclude logs/*'.format(sys.argv[1]))
-
-			# hit the kill switch and start countdown again
-			check_call('aws sns publish --topic-arn {0} --message {{}} --region {1}'.format(sys.argv[2], sys.argv[3]))
-			os.remove("/tmp/mc_last_activity")
+                # Trigger server shutdown
+                check_call(f'aws sns publish --topic-arn {sys.argv[2]} --message "{{}}" --region {sys.argv[3]}')
+                os.remove(last_activity_file)
 else:
-	# start the clock
-	f = open('/tmp/mc_last_activity', 'w')
-	f.write(str(time.time()))
+    # Initialize activity timestamp
+    with open(last_activity_file, 'w') as f:
+        f.write(str(time.time()))
