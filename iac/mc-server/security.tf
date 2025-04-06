451c45e91b2fcd948a08cf004aa2e87308034a6e
CatLogicDigital <hello@catlogic.digital>
2025-04-06 01:14:56 +0100

    Update security.tf

--- iac/mc-server/security.tf
+++ iac/mc-server/security.tf
@@ -1,20 +1,20 @@
 resource "aws_security_group" "minecraft" {
   vpc_id      = aws_vpc.minecraft.id
-  description = "Allow minecraft and ssh"
+  description = "Allow Minecraft and SSH from my IP"
   tags        = local.common_tags
 
   ingress {
     from_port   = 25565
     to_port     = 25565
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["195.144.8.62/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["195.144.8.62/32"]
   }
 
   egress {
@@ -24,4 +24,3 @@ resource "aws_security_group" "minecraft" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
