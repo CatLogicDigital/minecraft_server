aaa4129bb51d24bc84db83c37738e26e9fc00dab
CatLogicDigital <hello@catlogic.digital>
2025-04-06 15:44:46 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -20,7 +20,11 @@ mkdir minecraft
 # aws configure set default.s3.multipart_threshold 128MB
 # aws configure set default.s3.multipart_chunksize 64MB
 # Sync the backup to the server
-aws s3 sync s3://$1 minecraft/ --cli-read-timeout 0 --cli-connect-timeout 0 --exclude 'logs/*'
+# aws s3 sync s3://$1 minecraft/ --cli-read-timeout 0 --cli-connect-timeout 0 --exclude 'logs/*'
+# copy the backup zip over
+aws s3 cp s3://$1/2025_04_06_minecraft_backup.zip minecraft-backup.zip --no-progress --cli-read-timeout 0 --cli-connect-timeout 0
+# unzip the backup
+unzip -o minecraft-backup.zip -d minecraft
 
 # install minecraft if this is the first time
 if [ ! -f "minecraft/eula.txt" ]; then
