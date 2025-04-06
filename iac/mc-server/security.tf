700fbfbd331a6c883c581f8e4d92fc58a02c69f8
CatLogicDigital <hello@catlogic.digital>
2025-04-06 01:45:35 +0100

    Update security.tf

--- iac/mc-server/security.tf
+++ iac/mc-server/security.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "minecraft" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["195.144.8.62/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
