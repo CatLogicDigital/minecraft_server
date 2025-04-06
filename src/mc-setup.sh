87ea0cdcaf4a982d5c4e0853108fb9675f5827df
CatLogicDigital <hello@catlogic.digital>
2025-04-06 19:05:05 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -75,9 +75,9 @@ EOF
 ###(crontab -l 2>/dev/null; echo "* * * * * PATH=$PATH:/usr/local/bin python3 auto-shutoff.py s3://$1 $2 $3") | crontab -
 
 # Start Minecraft in a named screen session called "minecraft"
-# start minecraft (does not return)
-# screen -S minecraft -dm java -Xmx1024M -Xms1024M -jar server.jar nogui
-screen java -Xmx1024M -Xms1024M -jar server.jar nogui
+#screen -S minecraft -dm java -Xmx1024M -Xms1024M -jar server.jar nogui
+#screen java -Xmx1024M -Xms1024M -jar server.jar nogui ### works
+screen -S minecraft -dm bash -c "java -Xmx1024M -Xms1024M -jar server.jar nogui"
 
 
 echo "Server is ready :3"
