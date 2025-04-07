9576f74a6f4b104609b5e39b1fb9683ef1ac4b79
CatLogicDigital <hello@catlogic.digital>
2025-04-07 15:50:53 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -76,7 +76,7 @@ EOF
 
 # Start Minecraft in a named screen session called "minecraft"
 #screen -S minecraft -dm java -Xmx1024M -Xms1024M -jar server.jar nogui
-screen java -Xmx1024M -Xms1024M -jar server.jar nogui ### works
+screen java -Xmx1024M -Xms1024M -jar server.jar nogui
 #screen -S minecraft -dm bash -c "java -Xmx1024M -Xms1024M -jar server.jar nogui"
 
 
