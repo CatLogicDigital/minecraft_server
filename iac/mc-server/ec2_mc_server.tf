5ca7b635220268ff2d0b0139dca1549701207541
CatLogicDigital <hello@catlogic.digital>
2025-04-08 21:09:49 +0100

    Update ec2_mc_server.tf

--- iac/mc-server/ec2_mc_server.tf
+++ iac/mc-server/ec2_mc_server.tf
@@ -1,61 +1,162 @@
 # ------------------------------------
-# Add IAM Role and Policy for EC2 Termination
-# ------------------------------------
-resource "aws_iam_role" "ec2_termination_role" {
-  name = "ec2-termination-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  # Minecraft EC2 server
+  # ------------------------------------
+ 
+  # import existing ec2 keypair
+  data "aws_key_pair" "existing" {
+    key_name = var.ec2-key-pair-name
+  }
+ 
+  # import s3 used for backup
+  data "aws_s3_bucket" "mc_backup" {
+    bucket = var.mc-backup-bucket-name
+  }
+ 
+  resource "aws_instance" "minecraft" {
+    instance_type = "t2.medium"
+ 
+    ami               = var.ami-images[var.aws-region]
+    security_groups   = [aws_security_group.minecraft.id]
+    availability_zone = var.aws-zones[var.aws-region]
+    key_name          = data.aws_key_pair.existing.key_name
+    depends_on        = [aws_internet_gateway.minecraft]
+    subnet_id         = aws_subnet.minecraft.id
+ 
+    iam_instance_profile = aws_iam_instance_profile.minecraft.name
+ 
+    root_block_device {
+      volume_type = "standard"
+      volume_size = 40
+    }
+ 
+    tags        = local.common_tags
+    volume_tags = local.common_tags
+  }
+ 
+  resource "aws_eip_association" "minecraft" {
+    instance_id   = aws_instance.minecraft.id
+    allocation_id = data.aws_eip.mc_ip.id
+  }
+ 
+  # ------------------------------------
+  # IAM Roile for mincraft world backup on S3
+  # ------------------------------------
+  resource "aws_iam_instance_profile" "minecraft" {
+    name = "mc-backup"
+    role = "${aws_iam_role.minecraft.name}"
+  }
+ 
+  resource "aws_iam_role" "minecraft" {
+    name = "mc-backup-role"
+    path = "/"
+ 
+    assume_role_policy = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Action": "sts:AssumeRole",
+              "Principal": {
+                 "Service": "ec2.amazonaws.com"
+              },
+              "Effect": "Allow",
+              "Sid": ""
+          }
+      ]
+  }
+  EOF
+ 
+    tags = local.common_tags
+  }
+ 
+  resource "aws_iam_role_policy" "minecraft" {
+    name = "mc-backup-publish-policy"
+    role = "${aws_iam_role.minecraft.id}"
+ 
+    policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "s3:*"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "ec2_termination_policy" {
-  name = "ec2-termination-policy"
-  role = aws_iam_role.ec2_termination_role.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "ec2:TerminateInstances",
-      "Resource": "*",
-      "Condition": {
-        "StringEquals": {
-          "ec2:ResourceTag/Name": "minecraft-server"
-        }
+      {
+        "Action": [
+          "SNS:Publish"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
       }
+    ]
+  }
+  EOF
+  }
+ 
+  # -----------------------------------------
+  # Provision the minecraft server using remote-exec
+  # -----------------------------------------
+  resource "null_resource" "minecraft" {
+    triggers = {
+      public_ip = aws_eip_association.minecraft.public_ip
     }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "ec2_termination_instance_profile" {
-  name = "ec2-termination-instance-profile"
-  role = aws_iam_role.ec2_termination_role.name
-}
-
-# ------------------------------------
-# Update EC2 Instance (Attach IAM Role)
-# ------------------------------------
-resource "aws_instance" "minecraft" {
-  # Existing configurations...
-
-  # Attach the new IAM instance profile for termination
-  iam_instance_profile = aws_iam_instance_profile.ec2_termination_instance_profile.name
-
-  # Other existing configurations remain unchanged
-}
+ 
+    connection {
+      type        = "ssh"
+      host        = aws_eip_association.minecraft.public_ip
+      user        = "ec2-user"
+      port        = "22"
+      private_key = file("${path.module}/minecraft-key.pem")
+    }
+ 
+    // copy pre-configured ec2 instance private key
+    provisioner "file" {
+      source      = file("${path.module}/minecraft-key.pem")
+      destination = "id_rsa"
+    }
+   provisioner "file" {
+     source      = "${path.module}/minecraft-key.pem"
+     destination = "id_rsa"
+   }
+ 
+    // copy mc auto-shutoff function
+    provisioner "file" {
+      source      = "../../src/auto-shutoff.py"
+      destination = "auto-shutoff.py"
+    }
+ 
+    // copy mc deployment and start script
+    provisioner "file" {
+      source      = "../../src/mc-setup.sh"
+      destination = "mc-setup.sh"
+    }
+    provisioner "file" {
+      source      = "../../src/mc-server.sh"
+      destination = "mc-server.sh"
+    }
+ 
+    // copy tf config and var template
+    provisioner "file" {
+      source      = "./config.tf"
+      destination = "config.tf"
+    }
+    provisioner "file" {
+      source      = "./variables.tf"
+      destination = "variables.tf"
+    }
+    provisioner "file" {
+      source      = "../../config/account.tfvars"
+      destination = "account.tfvars"
+    }
+ 
+    // install minecraft and sync backup
+    provisioner "remote-exec" {
+      inline = [
+        "chmod a+x mc-*.sh",
+        "./mc-setup.sh ${data.aws_s3_bucket.mc_bucket.id} ${data.aws_sns_topic.mc_shutoff.arn} ${var.aws-region}"
+      ]
+    }
+  }
