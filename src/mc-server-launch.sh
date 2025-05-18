0b9ba7fbb8ccfc2725e9c196ebae72d5fe943457
CatLogicDigital <hello@catlogic.digital>
2025-05-18 18:57:06 +0100

    Update mc-server-launch.sh

--- src/mc-server-launch.sh
+++ src/mc-server-launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd /home/ec2-user/minecraft || exit 1
 
-screen -dmS minecraft bash -c "cd /home/ec2-user/minecraft && while true; do
+while true; do
   echo 'Launching Minecraft server...' >> minecraft.log
   java -Xmx1024M -Xms1024M -jar server.jar nogui >> minecraft.log 2>&1
   echo 'Server crashed or stopped. Restarting in 5 seconds...' >> minecraft.log
   sleep 5
-done"
+done
