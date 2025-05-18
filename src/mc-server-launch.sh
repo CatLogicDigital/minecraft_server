63a5d105f83d3b57a0f2b4f0202b931b03f9048c
CatLogicDigital <hello@catlogic.digital>
2025-05-18 18:23:39 +0100

    Update mc-server-launch.sh

--- src/mc-server-launch.sh
+++ src/mc-server-launch.sh
@@ -3,11 +3,9 @@
 # Set absolute working directory
 cd /home/ec2-user/minecraft || exit 1
 
-# Log the startup
-echo "Starting Minecraft server with auto-restart in screen..." >> minecraft.log
-
 # Launch Minecraft server with auto-restart loop
 while true; do
+  echo "Starting Minecraft server with auto-restart in screen..." >> minecraft.log
   screen -dmS minecraft java -Xmx1024M -Xms1024M -jar server.jar nogui >> minecraft.log 2>&1
   echo 'Server crashed or stopped. Restarting in 10 seconds...' >> minecraft.log
   sleep 10
