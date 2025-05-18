e59067edcff98b30821fc529bf4aed077ccfcabf
CatLogicDigital <hello@catlogic.digital>
2025-05-18 18:14:54 +0100

    Update mc-server-launch.sh

--- src/mc-server-launch.sh
+++ src/mc-server-launch.sh
@@ -7,14 +7,9 @@ cd /home/ec2-user/minecraft || exit 1
 echo "Starting Minecraft server with auto-restart in screen..." >> minecraft.log
 
 # Launch Minecraft server in a detached screen session with auto-restart loop
-screen -dmS minecraft bash -c "while true; do
+while true; do
   echo 'Launching Minecraft server...' >> minecraft.log
   java -Xmx1024M -Xms1024M -jar server.jar nogui >> minecraft.log 2>&1
   echo 'Server crashed or stopped. Restarting in 10 seconds...' >> minecraft.log
   sleep 10
-done"
-
-# Note: To stop the server loop:
-# Reattach: screen -r minecraft
-# Stop Minecraft with stop in console.
-# Exit the script with Ctrl+C or kill the screen: screen -S minecraft -X quit
+done
