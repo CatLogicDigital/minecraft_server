3da99ec4628ccb52312278dfbc5d52e9b618e441
CatLogicDigital <hello@catlogic.digital>
2025-06-22 20:02:59 +0100

    Update mc-server-launch.sh

--- src/mc-server-launch.sh
+++ src/mc-server-launch.sh
@@ -14,14 +14,13 @@ cd /home/ec2-user/minecraft || exit 1
 
 cat > start-loop.sh <<'EOF'
 #!/bin/bash
-#cd /home/ec2-user/minecraft || exit 1
-#while true; do
-#re add indent ##
-echo 'Launching Minecraft server...' >> minecraft.log
-java -Xmx1024M -Xms1024M -jar server.jar nogui
-echo 'Server crashed or stopped. Restarting in 5 seconds...' >> minecraft.log
-sleep 5
-#done
+cd /home/ec2-user/minecraft || exit 1
+while true; do
+  echo 'Launching Minecraft server...' >> minecraft.log
+  java -Xmx1024M -Xms1024M -jar server.jar nogui
+  echo 'Server crashed or stopped. Restarting in 5 seconds...' >> minecraft.log
+  sleep 5
+done
 EOF
 
 chmod +x start-loop.sh
