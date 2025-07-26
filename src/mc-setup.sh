44aafa11422b7933dbce974b7374a639d240daa6
CatLogicDigital <hello@catlogic.digital>
2025-07-26 18:47:11 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -73,7 +73,7 @@ xaeros-minimap
 EOF
 
     echo "Installing MODS via Modrinth API"
-    python3 mcsmp.py directory-add "minecraft" "/home/ec2-user/minecraft"
+    python3 mcsmp.py directory-add "/home/ec2-user/minecraft" "minecraft"
     python3 mcsmp.py version minecraft 1.21.7
     python3 mcsmp.py loader minecraft neoforge
 
