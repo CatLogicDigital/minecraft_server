181de1d8027c1eca731e690ab2169b7105bb8711
CatLogicDigital <hello@catlogic.digital>
2025-07-26 17:43:44 +0100

    Update mc-setup.sh

--- src/mc-setup.sh
+++ src/mc-setup.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 flavour="neoforge"
-#vanilla
-#neoforge
+# vanilla
+# neoforge
 
 minecraft_jar='https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar' # 1.21.8 # https://jars.vexyhost.com/
 
@@ -13,8 +15,8 @@ echo $FLAVOUR
 sudo rpm --import https://yum.corretto.aws/corretto.key
 sudo curl -Lo /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
 sudo yum install -y java-21-amazon-corretto-devel
-# install tmux
-sudo yum install -y tmux
+# install tmux, unzip, python3
+sudo yum install -y tmux unzip python3 python3-pip
 
 # copy tf templates to minecraft backup bucket
 aws s3 cp config.tf s3://$1
@@ -36,10 +38,11 @@ if [ "$flavour" = "vanilla" ]; then
     # install minecraft
     # navigate into mincraft dir
     echo "Installing Minecraft"
-        wget "$minecraft_jar" -O server.jar
-        java -Xmx1024M -Xms1024M -jar server.jar nogui
+    wget "$minecraft_jar" -O server.jar
+    java -Xmx1024M -Xms1024M -jar server.jar nogui
     echo "### Accepting EULA"
     echo "eula=true" > eula.txt
+
 elif [ "$flavour" = "neoforge" ]; then
     echo "Installing NeoForge"
     wget "$minecraft_jar" -O server.jar
@@ -52,32 +55,31 @@ elif [ "$flavour" = "neoforge" ]; then
     echo "eula=true" > eula.txt
 
     mkdir -p mods # create the mods folder
-    echo "MODS --- Installing WorldEdit for NeoForge"
-    #https://modrinth.com/plugin/worldedit?version=1.21.7&loader=neoforge
-    wget -O mods/worldedit-mod-7.3.15.jar https://cdn.modrinth.com/data/1u6JkXh5/versions/6stG33I5/worldedit-mod-7.3.15.jar
-    echo "MODS --- Installing Just Enough Items for NeoForge"
-    #https://modrinth.com/mod/jei?version=1.21.7&loader=neoforge
-    wget -O mods/jei-1.21.7-neoforge-23.1.0.4.jar https://cdn.modrinth.com/data/u6dRKJwZ/versions/Cp9YPdzb/jei-1.21.7-neoforge-23.1.0.4.jar
-    echo "MODS --- Installing Waystones for NeoForge"
-    #https://modrinth.com/mod/waystones?version=1.21.7&loader=neoforge
-    wget -O mods/waystones-neoforge-1.21.7-21.7.1.jar https://cdn.modrinth.com/data/LOpKHB2A/versions/CYru1h3x/waystones-neoforge-1.21.7-21.7.1.jar
-    echo "MODS --- Installing What The Hell Is That? for NeoForge"
-    #https://modrinth.com/mod/waystones?version=1.21.7&loader=neoforge
-    wget -O mods/wthit-neo-16.0.1.jar https://cdn.modrinth.com/data/6AQIaxuO/versions/tWbt6XcK/wthit-neo-16.0.1.jar
-    echo "MODS --- Mob Loassos for NeoForge"
-    https://modrinth.com/mod/mob-lassos?version=1.21.7&loader=neoforge#download
-    wget -O mods/MobLassos-v21.7.0-1.21.7-NeoForge.jar https://cdn.modrinth.com/data/ftOBbnu8/versions/2MCxG8Tj/MobLassos-v21.7.0-1.21.7-NeoForge.jar
-    echo "MODS --- Xaero's Minimap for NeoForge"
-    https://modrinth.com/mod/xaeros-minimap?version=1.21.7&loader=neoforge#download
-    wget -O mods/Xaeros_Minimap_25.2.10_NeoForge_1.21.7.jar https://cdn.modrinth.com/data/1bokaNcj/versions/JWQzpqe6/Xaeros_Minimap_25.2.10_NeoForge_1.21.7.jar
-    #install depedancies
-    echo "MODS --- Installing Balm (required by Waystones)"
-    wget -O mods/balm-21.7.0.jar https://cdn.modrinth.com/data/qvIfYCYJ/versions/XL7zoeFz/balm-21.7.0.jar
-    echo "MODS --- Installing BadPackets (required by WTHIT)"
-    wget -O mods/badpackets-forge-0.9.0.jar https://cdn.modrinth.com/data/WL2x6fy6/versions/LF1C3uHO/badpackets-forge-0.9.0.jar
-    echo "MODS --- Installing PuzzlesLib (required by MobLassos)"
-    wget -O mods/puzzleslib-1.21.7-21.7.1.jar https://cdn.modrinth.com/data/P7dR8mSH/versions/hDbfngXA/puzzleslib-1.21.7-21.7.1.jar
 
+    echo "Installing MC-Modrinth-Project-Manager"
+    pip3 install requests
+    curl -Lo mcsmp.py https://raw.githubusercontent.com/un-pogaz/MC-Modrinth-Project-Manager/main/mcsmp.py
+    chmod +x mcsmp.py
+
+    echo "Writing mod list to mods.txt"
+    cat > mods.txt <<EOF
+worldedit
+jei
+waystones
+wthit
+mob-lassos
+xaeros-minimap
+EOF
+
+    echo "Installing MODS via Modrinth API"
+    python3 mcsmp.py directory_add minecraft "$(pwd)"
+    python3 mcsmp.py version minecraft 1.21.7
+    python3 mcsmp.py loader minecraft neoforge
+
+    while IFS= read -r mod; do
+        echo "Installing MOD: $mod"
+        python3 mcsmp.py install minecraft "$mod"
+    done < mods.txt
 fi
 
 echo "Setting server properties"
